7755a5cba6d7a37bfcce255a1a6a7b9487561a08
James ABIB <james.bidule@gmail.com>
2023-12-16 12:14:04 +0100

    refacto: add instollowing class
    This commit adds a new class called "Instollowing" to the code. The class has several methods for parsing JSON files, creating output folders and files, and generating HTML files. It also includes a method for getting followers and following from Instagram. The main function in "instollowing.py" now creates an instance of the "Instollowing" class and calls its "run" method.

--- fun.py
+++ fun.py
@@ -5,132 +5,115 @@ import webbrowser
 import instaloader as IL
 from instaloader import instaloader
 
-outDir = 'out'
-directory = outDir + '/whoIsNotFollowingBack.csv'
-instagramLink = 'https://www.instagram.com/'
-imgUrl = "logo.jpg"
 
+class Instollowing:
+    def __init__(self, username=None, password=None):
+        self.username = username
+        self.password = password
+        if self.username is not None or self.password is not None:
+            self.ig = IL.Instaloader()
+            self.ig.login(username, password)
+        self.outDir = 'out'
+        self.directory = self.outDir + '/whoIsNotFollowingBack.csv'
+        self.instagramLink = 'https://www.instagram.com/'
+        self.imgUrl = "logo.jpg"
 
-def parseJson(file):
-    items = list()
-    with open(file) as f:
-        data = json.load(f)
-        typeRelationShips = 'relationships_following' if 'relationships_following' in data else 'relationships_follower'
+    def parseJson(self, file):
+        items = list()
+        with open(file) as f:
+            data = json.load(f)
+            typeRelationShips = 'relationships_following' if 'relationships_following' in data else 'relationships_follower'
 
-        for i in data[typeRelationShips]:
-            for j in i['string_list_data']:
-                item = {
-                    'username': j['value'],
-                    'url': j['href'],
-                    'imgUrl': imgUrl
-                }
-                items.append(item)
-    return sortList(items)
+            for i in data[typeRelationShips]:
+                for j in i['string_list_data']:
+                    item = {
+                        'username': j['value'],
+                        'url': j['href'],
+                        'imgUrl': self.imgUrl
+                    }
+                    items.append(item)
+        return self.sortList(items)
 
+    def sortList(self, items):
+        return sorted(items, key=lambda x: x['username'])
 
-def sortList(items):
-    return sorted(items, key=lambda x: x['username'])
+    def createOutFolder(self):
+        if not os.path.exists(self.outDir):
+            os.makedirs(self.outDir)
 
+    def createCSVFile(self):
+        self.createOutFolder()
+        if not os.path.exists(self.directory):
+            with open(self.directory, 'w') as f:
+                f.write('')
+        else:
+            os.remove(self.directory)
+            self.createCSVFile()
+        return
 
-def createOutFolder():
-    if not os.path.exists(outDir):
-        os.makedirs(outDir)
+    def createHTMLFile(self):
+        file_html = open(f"{self.outDir}/whoIsNotFollowingBack.html", "w")
+        file_html.write('''<html>
+        <head>
+        <title>Instollowing</title>
+        </head> 
+        <body>
+        <h1>That's who is not following you back</h1>     
+        <ul>
+        ''')
+        with open(self.directory, 'r') as csvfile:
+            reader = csv.reader(csvfile, delimiter=';')
+            for row in reader:
+                if row[0] == 'username': continue
 
+                line = ('<img src="' + row[2] + '" width="50" height="50">') + (
+                        '<a href="' + row[1] + '">' + row[0] + '</a>') if row[2] != 'none' else (
+                        '<a href="' + row[1] + '">' + row[0] + '</a>')
 
-def createOutFile():
-    createOutFolder()
-    if not os.path.exists(directory):
-        with open(directory, 'w') as f:
-            f.write('')
-    else:
-        os.remove(directory)
-        createOutFile()
-    return
-
-
-def createHTMLFile():
-    file_html = open(f"{outDir}/whoIsNotFollowingBack.html", "w")
-    file_html.write('''<html>
-    <head>
-    <title>Instollowing</title>
-    </head> 
-    <body>
-    <h1>That's who is not following you back</h1>     
-    <ul>
+                file_html.write('<li>' + line + '</li>')
+                file_html.write('\n     ')
+        file_html.write('''
+        </ul>
+      </body>
+    </html>
     ''')
-    with open(directory, 'r') as csvfile:
-        reader = csv.reader(csvfile, delimiter=';')
-        for row in reader:
-            if row[0] == 'username': continue
+        file_html.close()
+        url = 'file://' + os.path.realpath(file_html.name)
+        webbrowser.open(url, new=2)
 
-            line = ('<img src="' + row[2] + '" width="50" height="50">') + (
-                    '<a href="' + row[1] + '">' + row[0] + '</a>') if row[2] != 'none' else (
-                    '<a href="' + row[1] + '">' + row[0] + '</a>')
+    def getFollowersAndFollowing(self):
+        profile = instaloader.Profile.from_username(self.ig.context, self.username)
 
-            file_html.write('<li>' + line + '</li>')
-            file_html.write('\n     ')
-    file_html.write('''
-    </ul>
-  </body>
-</html>
-''')
-    file_html.close()
-    url = 'file://' + os.path.realpath(file_html.name)
-    webbrowser.open(url, new=2)
+        followers_list, following_list = list(profile.get_followers()), list(profile.get_followees())
+        followers, following = [], []
 
+        for follower in followers_list:
+            followers.append(follower.username + ";" + self.instagramLink + follower.username + ";none")
 
-# Actually this function is not fully working because of the 2FA
-def loginInstagram():
-    with open('credentials.json') as f:
-        credentials = json.load(f)
+        for followee in following_list:
+            following.append(followee.username + ';' + self.instagramLink + followee.username)
 
-    username = credentials['username']
-    password = credentials['password']
+        followers = sorted(followers)
+        following = sorted(following)
 
-    il = IL.Instaloader()
+        return following, followers
 
-    try:
-        il.login(username, password)
-        print("Successfully logged in.")
-    except instaloader.TwoFactorAuthRequiredException:
-        code_2fa = input("2FA Code : ")
-        il.two_factor_login(code_2fa)
+    def whoIsNotFollowingBack(self, following, followers):
+        self.createCSVFile()
+        usernames_followers = {f['username'] for f in followers}
 
-    return il
+        for user in following:
+            if user['username'] not in usernames_followers:
+                with open(self.directory, 'a') as f:
+                    user_info = f"{user['username']};{user['url']};{user['imgUrl']}"
+                    f.write(user_info + '\n')
 
+        self.createHTMLFile()
 
-def getFollowersAndFollowing():
-    usernameToCheck = "abib_james"
-    il = loginInstagram()
-    profile = instaloader.Profile.from_username(il.context, usernameToCheck)
+    def run(self):
+        if self.username is None or self.password is None:
+            following, followers = self.parseJson('following.json'), self.parseJson('followers.json')
+        else:
+            following, followers = self.getFollowersAndFollowing()
 
-    followers_list, following_list = list(profile.get_followers()), list(profile.get_followees())
-    followers, following = [], []
-
-    for follower in followers_list:
-        followers.append(follower.username + ";" + instagramLink + follower.username + ";none")
-
-    for followee in following_list:
-        following.append(followee.username + ';' + instagramLink + followee.username)
-
-    followers = sorted(followers)
-    following = sorted(following)
-
-    return following, followers
-
-
-def whoIsNotFollowingBack(following, followers):
-    createOutFile()
-    usernames_followers = {f['username'] for f in followers}
-
-    for user in following:
-        print(user)
-        if user['username'] not in usernames_followers:
-            with open(directory, 'a') as f:
-                if os.stat(directory).st_size == 0:
-                    f.write('username;url;imgUrl\n')
-
-                user_info = f"{user['username']};{user['url']};{user['imgUrl']}"
-                f.write(user_info + '\n')
-
-    createHTMLFile()
+        self.whoIsNotFollowingBack(following, followers)
